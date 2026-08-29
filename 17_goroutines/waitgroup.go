f26e9579c62b160b6e7963c5ed71b299ff996430
srikanta0427 <srikanta0427@gmail.com>
2026-08-29 23:20:43 +0530

    "day 6"

--- 17_goroutines/waitgroup.go
+++ 17_goroutines/waitgroup.go
@@ -3,16 +3,18 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 func tasks[T int | string](arg T, wg *sync.WaitGroup) {
 	defer wg.Done()
 	fmt.Println("tasks started", arg)
+	time.Sleep(time.Second * 2)
 }
 
 func main() {
 	var wg sync.WaitGroup
-	wg.Add(2)
+	wg.Add(1)
 	go tasks("Hello", &wg)
 	go tasks("world", &wg)
 	wg.Wait()
