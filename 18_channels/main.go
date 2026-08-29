63736aeaef1587a44a2d8bb715861169f37a5361
srikanta0427 <srikanta0427@gmail.com>
2026-08-29 23:21:35 +0530

    day 06

---
+++ 18_channels/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func processNum(msg chan string,) {
+	result := "some result"
+	time.Sleep(time.Second * 2)
+	msg <- result
+}
+
+func main() {
+
+	msgChain := make(chan string)
+	go processNum(msgChain,)
+	msg := <-msgChain
+	fmt.Println(msg)
+
+	fmt.Println("work after receiving msg(chain)")
+
+}
