028ba4837128b0ee339a2a86d445e85c9dddfeb7
srikanta0427 <srikanta0427@gmail.com>
2026-08-30 00:34:11 +0530

    day 06

---
+++ 18_channels/buffered_channel.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+func task1() string {
+	return "task 1 complete"
+}
+
+func task2() string {
+	return "task 2 complete"
+}
+
+func worker(ch chan string) {
+	ch <- task1()
+	ch <- task2()
+	ch <- "task 3 complete"
+}
+
+func main() {
+	var ch = make(chan string, 2)
+
+	go worker(ch)
+	//msg := <-ch
+	fmt.Println("before work")
+	fmt.Println(<-ch)
+	//fmt.Println(msg)
+	fmt.Println("after work")
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+}
